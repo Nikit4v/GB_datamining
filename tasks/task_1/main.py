fc7ccdfbd27d3ded560758c122e2c5554bd2785c
Diabl <dath.vg@gmail.com>
2021-04-01 05:49:32 +0300

    Added multithreading

--- tasks/task_1/main.py
+++ tasks/task_1/main.py
@@ -1,3 +1,4 @@
+import threading
 import requests
 import json
 
@@ -6,19 +7,28 @@ API_URL = "https://5ka.ru/api/v2/"
 
 
 # ## FUNCTIONS ## #
+def thread(category):
+    now_page = requests.get(
+        API_URL + "special_offers?records_per_page=20&categories=" + category["parent_group_code"]).json()
+    buffer = now_page["results"]
+    while now_page["next"]:
+        now_page = requests.get(now_page["next"]).json()
+        buffer += now_page["results"]
+    with open(category["parent_group_code"] + ".json", 'w') as f:
+        json.dump({
+            "name": category["parent_group_name"],
+            "code": category["parent_group_code"],
+            "products": buffer
+        }, f)
+
+
 def get_special_offers():
+    threads = []
     for category in requests.get(API_URL + "categories").json():
-        now_page = requests.get(API_URL + "special_offers?categories=" + category["parent_group_code"]).json()
-        buffer = now_page["results"]
-        while now_page["next"]:
-            now_page = requests.get(now_page["next"]).json()
-            buffer += now_page["results"]
-        with open(category["parent_group_code"]+".json", 'w') as f:
-            json.dump({
-                    "name": category["parent_group_name"],
-                    "code": category["parent_group_code"],
-                    "products": buffer
-                }, f)
+        threads.append(tr := threading.Thread(target=thread, args=(category,)))
+        tr.start()
+    for tr in threads:
+        tr.join()
 
 
 # ## MAIN ## #
